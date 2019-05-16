9e47d6c5cc6f3ff3405659fb060b2fd0aaf1ad0b
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-05-16 15:21:45 +0200

    remove Array.from()

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -22,7 +22,7 @@ class TabLink {
     const links = document.querySelectorAll(`.tabs-link`);
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(links).forEach( link => link.classList.remove('tabs-link-selected'));
+    links.forEach( link => link.classList.remove('tabs-link-selected'));
 
     // Add a class named "tabs-link-selected" to this link
     this.element.classList.add("tabs-link-selected");
@@ -42,7 +42,7 @@ class TabItem {
     // Select all ".tabs-item" elements from the DOM
     const items = document.querySelectorAll(".tabs-item");
     // Remove the class "tabs-item-selected" from each element
-    Array.from(items).forEach( tab => tab.classList.remove('tabs-item-selected'));
+    items.forEach( tab => tab.classList.remove('tabs-item-selected'));
     // Add a class named "tabs-item-selected" to this element
     this.element.classList.add("tabs-item-selected");
   }
